69db624b875fb24ad9f824fc405bf184682bd62f
ashaginaw <alshaginaw@gmail.com>
2022-06-18 11:00:03 -0400

    New Changes Made to the To Do page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,31 +1,31 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import "./About.css";
-import profile_pic from "../assets/profile_pic.jpg";
-
+import profilepicture_shaginaw from "../assets/profilepicture_shaginaw.jpg";
+  
 export default class About extends Component {
   render() {
     return (
       <div>
-        {/* <p>Design your About me page </p> */}
-        <div class="split left">
-          <div className="centered">
-            <img
-              className="profile_image"
-              src={profile_pic}
-              alt="Profile Pic"
-            ></img>
-          </div>
-        </div>
-        <div className="split right">
-          <div className="centered">
-            <div className="name_title">Your Name</div>
-            <div className="brief_description">
-              Tell us about yourself in a few sentences. Tell us your interests
-              and say a fun fact about yourself.
-            </div>
-          </div>
+        <p> <div>
+    <div class="split left">
+      <div className="centered">
+        <img 
+          className="profile_image"
+          src={profilepicture_shaginaw}
+          alt="Profile Pic"
+          ></img>
+      </div>
+    </div>
+    <div className="split right">
+      <div className="centered">
+        <div className="name_title">Amanda Shaginaw</div>
+        <div className="brief_description">
+          Hello! I am currently studying Computer Science at Duquesne University. I am also a Service Desk Analyst III, assisting with behind-the-scene applications and student/staff permissions. One day I hope to work at IBM working with Artificial Intelligence. A fun fact: I am internationally ranked for speedrunning Pokemon: The Trading Card Game (currently 61st)!
         </div>
       </div>
-    );
+    </div>
+  </div> </p> 
+      </div>
+    )
   }
 }
